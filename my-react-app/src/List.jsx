344b13f69ec9fca39e7ec00489a1e622a2926c68
hamsa30 <hoodwingg@gmail.com>
2024-03-20 17:44:04 +0530

    Episode 1

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,16 @@
+function List(props){
+    const itemList = props.items;
+    const category = props.category;
+
+
+    const listItems = itemList.map(item=> <li key="item.id">
+                                         {item.name}: &nbsp;
+                                         <b>{item.calories}</b></li>)
+    return(<>
+    <h3>{category}</h3>
+    <ol>{listItems}</ol>
+    </>
+    )
+}
+
+export default List
